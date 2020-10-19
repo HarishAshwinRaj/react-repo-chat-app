78a34d95ac2ac58838e19851640fe37636115ea9
HarishAshwinRaj <53622584+HarishAshwinRaj@users.noreply.github.com>
2020-10-19 15:51:01 +0530

    mic testing mic testing for flex box

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Home = () => {
-  return <div style={{}}>home</div>;
+  return (
+    <div style={{ color: "red", flexDirection: "row", display: "flex" }}>
+      <div>flex</div>
+      <div style={{ color: "red" }}>flex</div>
+      <div>flex</div>
+    </div>
+  );
 };
 
 export default Home;
